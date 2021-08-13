102ef3621c3ab5d87764996559d6bad936f95da7
Daniel Flanagan <dancharley8@gmail.com>
2021-08-12 23:15:49 -0700

    Linux and Windows Local IP, Public IP, MAC Adress

--- mac_remote/__init__.py
+++ mac_remote/__init__.py
@@ -1,7 +1,10 @@
 import socket
+import platform
 
 from getmac import get_mac_address
 import httpx
+
+from .getmac_linux_fix import get_linux_mac_from_local_ip
 
 def get_local_ip():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -9,8 +12,13 @@ def get_local_ip():
         local_ip = s.getsockname()[0]
     return local_ip
 
-def get_mac():
-    return get_mac_address(ip=get_local_ip())
+def get_mac(ip_address=get_local_ip()):
+    mac = get_mac_address(ip=ip_address)
+    if mac:
+        return mac
+    elif platform.system() == 'Linux':
+        return get_linux_mac_from_local_ip(ip_address)
+    
 
 def get_public_ip():
     request = httpx.get('http://ip-api.com/json')
@@ -22,4 +30,3 @@ def get_summary():
         'public': get_public_ip(),
         'mac': get_mac()
     }
-
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mac-remote-command",
-    version="0.0.1",
+    version="0.0.3",
     author="Daniel Flanagan",
     description="Client for embedded systems to facilitate remote configuration.",
     long_description=long_description,
